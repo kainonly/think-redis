200f10b395d4e89c430258ac6754742b9a340cbd
kain <kainpro@foxmail.com>
2019-02-28 00:48:01 +0800

    Update Traits and Common

---
+++ common/BitController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace lumen\bit\common;
+
+use Illuminate\Http\Request;
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+abstract class BitController extends BaseController
+{
+    protected $model;
+    protected $post = [];
+
+    protected $origin_lists_before_result = [
+        'error' => 1,
+        'msg' => 'error:before_fail'
+    ];
+    protected $origin_lists_condition = [];
+    protected $origin_lists_order_columns = 'create_time';
+    protected $origin_lists_order_direct = 'desc';
+    protected $origin_lists_columns = ['*'];
+
+    protected $lists_before_result = [
+        'error' => 1,
+        'msg' => 'error:before_fail'
+    ];
+    protected $lists_condition = [];
+    protected $lists_order_columns = 'create_time';
+    protected $lists_order_direct = 'desc';
+    protected $lists_columns = ['*'];
+
+    protected $get_validate = [
+        'id' => 'required|string|size:36'
+    ];
+    protected $get_before_result = [
+        'error' => 1,
+        'msg' => 'error:before_fail'
+    ];
+    protected $get_condition = [];
+    protected $get_select = ['*'];
+
+    protected $add_before_result = [
+        'error' => 1,
+        'msg' => 'error:before_fail'
+    ];
+    protected $add_after_result = [
+        'error' => 1,
+        'msg' => 'error:after_fail'
+    ];
+    protected $add_fail_result = [
+        'error' => 1,
+        'msg' => 'error:insert_fail'
+    ];
+
+    protected $edit_validate = [
+        'id' => 'required|string|size:36',
+        'switch' => 'required|bool'
+    ];
+    protected $edit_before_result = [
+        'error' => 1,
+        'msg' => 'error:before_fail'
+    ];
+    protected $edit_fail_result = [
+        'error' => 1,
+        'msg' => 'error:fail'
+    ];
+    protected $edit_after_result = [
+        'error' => 1,
+        'msg' => 'error:after_fail'
+    ];
+    protected $edit_switch = false;
+
+    protected $delete_validate = [
+        'id' => 'required|string|size:36',
+    ];
+    protected $delete_before_result = [
+        'error' => 1,
+        'msg' => 'error:before_fail'
+    ];
+    protected $delete_prep_result = [
+        'error' => 1,
+        'msg' => 'error:prep_fail'
+    ];
+    protected $delete_fail_result = [
+        'error' => 1,
+        'msg' => 'error:fail'
+    ];
+    protected $delete_after_result = [
+        'error' => 1,
+        'msg' => 'error:after_fail'
+    ];
+
+    public function __construct(Request $request)
+    {
+        $this->post = $request->toArray();
+    }
+}
---
+++ traits/GetModel.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace lumen\bit\traits;
+
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+/**
+ * Trait GetModel
+ * @package lumen\bit\traits
+ * @property string model
+ * @property array post
+ * @property array get_validate
+ * @property array get_before_result
+ * @property array get_condition
+ * @property array get_select
+ * @method validate(Request $request, array $rules, array $messages = [], array $customAttributes = [])
+ */
+trait GetModel
+{
+    public function get()
+    {
+        $validator = Validator::make($this->post, $this->get_validate);
+        if ($validator->fails()) return [
+            'error' => 1,
+            'msg' => $validator->errors()
+        ];
+
+        if (method_exists($this, '__getBeforeHooks') &&
+            !$this->__getBeforeHooks()) {
+            return $this->get_before_result;
+        }
+
+        try {
+            $condition = $this->get_condition;
+            if (isset($this->post['id']) &&
+                !empty($this->post['id'])) array_push(
+                $condition,
+                ['id', '=', $this->post['id']]
+            );
+
+            if (isset($this->post['where']) &&
+                !empty($this->post['where'])) $condition = array_merge(
+                $condition,
+                $this->post['where']
+            );
+
+            $data = Db::table($this->model)
+                ->where($condition)
+                ->first($this->get_select);
+
+            return method_exists($this, '__getCustomReturn') ? $this->__getCustomReturn($data) : [
+                'error' => 0,
+                'data' => $data
+            ];
+        } catch (QueryException $e) {
+            return [
+                'error' => 1,
+                'msg' => $e->errorInfo
+            ];
+        }
+    }
+}
